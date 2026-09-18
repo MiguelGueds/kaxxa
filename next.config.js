85f7287211cf351239ea7b6b080af5addacaff61
MiguelGueds <61873785+MiguelGueds@users.noreply.github.com>
2026-09-17 23:36:35 -0300

    00

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	webpack: (config, { isServer }) => {
+		config.resolve.fallback = {
+			...config.resolve.fallback,
+			canvas: false,
+		};
+		if (isServer) {
+			config.externals = [...(config.externals || []), 'canvas'];
+		}
+		return config;
+	},
 };
 
 module.exports = nextConfig;
